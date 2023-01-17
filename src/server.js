fb5c880bbb46dbe2c2f7f966c986e0836bfaea5c
Danilo Leão <olin_ad@hotmail.com>
2023-01-17 08:32:52 -0300

    initial config

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import http from 'http';
+import { MongoClient } from 'mongodb';
+import app from './app.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = 5000;
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const mongoCLient = new MongoClient(DATABASE_URL);
+let db;
+
+try {
+  await mongoCLient.connect();
+  db = mongoCLient.db();
+} catch (error) {
+  console.log('Error: ', error);
+  throw new Error(error);
+}
+
+const server = http.createServer(app);
+
+server.listen(PORT, console.log(`Listening on port ${PORT}...`));
