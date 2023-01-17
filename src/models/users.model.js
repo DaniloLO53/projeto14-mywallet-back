8e0780815be565e87c8399a16bb2e5f943da4a8f
Danilo Leão <olin_ad@hotmail.com>
2023-01-17 14:26:27 -0300

    bomb

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,12 +1,21 @@
 import db from "../server.js";
 
 async function addUser(user) {
+  const userSignedUp = await db.collection('users').findOne({ email: user.email });
   const alreadyExistHttpCode = 409;
-  const alreadyExist = await db.collection('users').findOne({ email: user.email });
 
-  if (alreadyExist) return alreadyExistHttpCode;
+  if (userSignedUp) return alreadyExistHttpCode;
 
-  return await db.collection('users').insertOne(user);
-}
+  return await db.collection('users').insertOne({ ...user, wallet: [] });
+};
 
-export { addUser };
+async function getUser(user) {
+  const userSignedUp = await db.collection('users').findOne({ email: user.email });
+  const accessDeniedCode = 403;
+
+  if (!userSignedUp) return accessDeniedCode;
+
+  return userSignedUp;
+};
+
+export { addUser, getUser };
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { httpAddUser } from "./users.controller.js";
+import { httpUpdateWallet, httpAddUser, httpGetUser } from "./users.controller.js";
 
 const usersRouter = express.Router();
 
 usersRouter.post('/', httpAddUser);
+usersRouter.get('/', httpGetUser);
+usersRouter.put('/', httpUpdateWallet);
 
 export default usersRouter;
