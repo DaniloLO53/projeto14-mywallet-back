192674feca50100c4ceb92cb2f51880922de84a9
Danilo Leão <olin_ad@hotmail.com>
2023-01-21 13:27:10 -0300

    edit income

--- src/routes/registers/register.controller.js
+++ src/routes/registers/register.controller.js
@@ -19,7 +19,7 @@ async function getRegisters(request, response) {
 
     const registers = await db.collection('registers').find({ userId: ObjectId(session.userId) }).toArray();
 
-    console.log('User registers: ', registers);
+    // console.log('User registers: ', registers[0].registers);
 
     return response.status(200).send(registers);
   } catch (error) {
@@ -63,4 +63,36 @@ async function addRegister(request, response, next) {
   }
 };
 
-export { getRegisters, addRegister };
+async function editRegister(request, response) {
+  const {
+    now,
+    value,
+    description,
+    type,
+    userId,
+    index: registerIndex,
+  } = request.body;
+
+  try {
+    const userRegisters = await db.collection('registers').find({
+      userId: ObjectId(userId),
+    }).toArray();
+
+    userRegisters[0].registers[registerIndex] = { now, value, description, type };
+
+    await db.collection('registers').updateOne(
+      { userId: ObjectId(userId) },
+      {
+        $set: { userId: ObjectId(userId), registers: userRegisters[0].registers },
+      },
+      { upsert: true },
+    )
+
+    return response.status(201).send(userId);
+  } catch (error) {
+    console.log('Error: ', error);
+    return response.status(500).send(error);
+  }
+};
+
+export { getRegisters, addRegister, editRegister };
--- src/routes/registers/register.router.js
+++ src/routes/registers/register.router.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { validateRegister } from '../../middewares/register.middleware.js';
-import { addRegister, getRegisters } from './register.controller.js';
+import { addRegister, editRegister, getRegisters } from './register.controller.js';
 
 const registerRouter = express.Router();
 
 registerRouter.get('/home', getRegisters);
 registerRouter.put('/nova-entrada', validateRegister, addRegister);
 registerRouter.put('/nova-saida', validateRegister, addRegister);
+registerRouter.put('/editar-entrada/:id', editRegister);
 
 export default registerRouter;
